4c07975bbe548759cc9dc9882cc5c44f6a0657d6
Bryce Russell <brycetrussell@gmail.com>
2024-04-22 10:11:44 -0500

    Add name to theme options

--- package/index.ts
+++ package/index.ts
@@ -2,6 +2,7 @@ import defineTheme from "astro-theme-provider";
 import { z } from "astro/zod";
 
 export default defineTheme({
+	name: "my-theme",
 	schema: z.object({
 		title: z.string(),
 		description: z.string().optional(),
